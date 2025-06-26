2677334fdbad76eea52c58a3d5204b8b8aca2784
Barna Toth <toakak@gmail.com>
2025-06-26 09:58:36 +0200

    v1.0.1

--- browser/src/lib/initializer.ts
+++ browser/src/lib/initializer.ts
@@ -1,7 +1,9 @@
 import { IStageFluxPublicAPI, SitePublicId, WindowWithStageFlux } from './public.interface';
 import { loadScriptAsync } from './utils/window.utils';
 
-export async function init(siteId: SitePublicId): Promise<IStageFluxPublicAPI> {
+export async function init(siteId: SitePublicId, options?: {
+  scriptUrl?: string;
+}): Promise<IStageFluxPublicAPI> {
   if (!window) {
     throw new Error('[StageFlux] window object not available');
   }
@@ -12,7 +14,15 @@ export async function init(siteId: SitePublicId): Promise<IStageFluxPublicAPI> {
     return stageFluxAPI;
   }
 
-  await loadScriptAsync(window, 'https://visitor.stageflux.com');
+  const scriptToLoad = options?.scriptUrl ?? 'https://visitor.stageflux.com';
+
+  try {
+    await loadScriptAsync(window, scriptToLoad);
+  } catch (e: any) {
+    throw new Error(`[StageFlux] Failed to load script: ${ scriptToLoad }`, {
+      cause: e,
+    });
+  }
 
   stageFluxAPI = (<WindowWithStageFlux><any>window).$StageFlux;
 
